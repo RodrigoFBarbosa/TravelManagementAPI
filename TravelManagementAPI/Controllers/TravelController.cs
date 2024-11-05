5c78698f67913d8b5a59984dac793f1710a88a7d
Rodrigo <rodrigobarbosa.p99@gmail.com>
2024-11-05 15:12:22 -0300

    feat(TravelController): adicionar controlador para Travel

---
+++ TravelManagementAPI/Controllers/TravelController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravelManagementAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TravelController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
