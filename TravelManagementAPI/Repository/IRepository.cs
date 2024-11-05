6ba4e9b02327c084f9679135b52cecfbe0ac8a14
Rodrigo <rodrigobarbosa.p99@gmail.com>
2024-11-05 15:11:47 -0300

    feat(repository): adicionar padrão Repository para manipulação de dados

---
+++ TravelManagementAPI/Repository/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace TravelManagementAPI.Repository;
+
+public interface IRepository<T> 
+{
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T> GetByIdAsync(int id);
+    T Add(T entity);
+    T Update(T entity);
+    T Delete(T entity);
+}
---
+++ TravelManagementAPI/Repository/Repository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TravelManagementAPI.Context;
+
+namespace TravelManagementAPI.Repository;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    protected readonly AppDbContext _context;
+    protected readonly DbSet<T> _dbSet;
+
+    public Repository(AppDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<T>();
+        
+    }
+
+    public T Add(T entity)
+    {
+        _dbSet.Add(entity);
+        return entity;
+    }
+
+    public T Delete(T entity)
+    {
+        _dbSet.Remove(entity);
+        return entity;
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+       return await _dbSet.AsNoTracking().ToListAsync();
+    }
+
+    public async Task<T> GetByIdAsync(int id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public T Update(T entity)
+    {
+         _dbSet.Update(entity);
+        return entity;
+    }
+}
